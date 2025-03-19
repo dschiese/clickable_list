20b88009c2260b467ec6f842a87dd95211c87563
Dennis Schiese <schiesedennis@googlemail.com>
2025-03-19 23:34:25 +0100

    Added callback function

--- src/clickable_list/__init__.py
+++ src/clickable_list/__init__.py
@@ -6,7 +6,14 @@ import streamlit.components.v1 as components
 frontend_dir = (Path(__file__).parent / "frontend").absolute()
 _component_func = components.declare_component("clickable_list", path=str(frontend_dir))
 
-def clickable_list(options: List[Dict[str, int]], indent=20 , style=None, key: Optional[str] = None):
+def clickable_list(
+    options: List[Dict[str, any]],
+    indent: int = 10,
+    key: Optional[str] = None,
+    on_change: Optional[callable] = None,
+    args: Optional[tuple] = (),
+    kwargs: Optional[dict] = None
+) -> Optional[str]:
     """
     Erstellt eine klickbare Liste in Streamlit.
 
@@ -16,4 +23,17 @@ def clickable_list(options: List[Dict[str, int]], indent=20 , style=None, key: O
     :param key: Optionaler Streamlit Key
     :return: Die ID des geklickten Items
     """
-    return _component_func(options=options, indent=indent, style=style, key=key)
+    component_value = _component_func(
+        options=options,
+        indent=indent,
+        key=key
+    )
+
+    # Handle on_change callback if defined
+    if component_value is not None and on_change:
+        if args or kwargs:
+            on_change(*args, **(kwargs or {}))
+        else:
+            on_change()
+
+    return component_value
